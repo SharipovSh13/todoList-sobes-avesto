e22374d73976d5cba9042be8b296d13d8969bce8
shuhrat <shyhratyagnobi2002@gmail>
2025-05-15 14:46:27 +0300

    Добавлен function delete

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import axios from 'axios';
 import { Check, Search, Trash, Trash2, UserCheck, UserX } from "lucide-react";
 import { useEffect, useState } from 'react'
 import './App.css'
+import LoadingComponent from './components/loading';
 
 function App() {
 
   let API = "https://jsonplaceholder.typicode.com/todos"
 
   const [todos, setTodos] = useState([])
+  const [loader, setLoader] = useState(true)
+  const [search, setSearch]= useState(``)
 
   async function getTodos() {
     try {
-      const { data } = await axios.get(`${API}`)
 
-      setTodos(data)
+      const { data } = await axios.get(`${API}`)
+      const newData = data.slice(0, 10)
+      setLoader(false)
+
+
+      setTodos(newData)
 
     } catch (error) {
       console.error(error);
@@ -24,6 +31,31 @@ function App() {
   }, []);
 
 
+  async function functionDeleteTodos(id) {
+    console.log(id);
+    setLoader(!loader)
+    try {
+      await axios.delete(`${API}/${id}`)
+      setTimeout(() => {
+
+        setLoader(true)
+      }, 500);
+
+      setTimeout(() => {
+        alert(`Удалeно id:${id}`, )
+      }, 4600);
+      
+
+
+
+
+    } catch (error) {
+      console.log(error);
+      setLoader(false)
+
+    }
+
+  }
 
 
 
@@ -33,8 +65,11 @@ function App() {
 
 
   return (
-    <>
+    <> {
+      loader && <LoadingComponent />
+    }
       <header className='bg-green-700/80 h-fit  p-4'>
+
 
         <div className=" w-[99%] h-fit   items-center  flex flex-col justify-between sm:flex-row ">
 
@@ -43,7 +78,7 @@ function App() {
           <div className="flex flex-row w-[90%]  sm:w-[40%]   items-center justify-evenly   h-10 p-1">
             <div className={`flex  items-center gap-1 relative `}>
 
-              <input type="search" placeholder='Search task..' className='h-[35pxh]   placeholder:p-1  bg-gray-200 hover:placeholder:opacity-55 border-spacing-0.5  rounded-[3px]' />
+              <input type="search" placeholder='Search task..' className='h-[35pxh]   placeholder:p-1  bg-gray-200 hover:placeholder:opacity-55 border-spacing-0.5  rounded-[3px]' onChange={(e)=> setSearch(e.target.value)}  />
               <Search size={18} className="absolute left-[85%]" />
             </div>
 
@@ -55,16 +90,9 @@ function App() {
 
         </div>
 
-
-
-
-
-
-
-
-
-
       </header>
+
+
       <main className=''>
         <div className='w-screen mt-4 p-4'>
 
@@ -72,6 +100,7 @@ function App() {
 
             <thead className='bg-gray-300/50 h-[40px]  '>
               <tr className=''>
+                <th>ID</th>
                 <th className='text-start p-1'>Заголовок</th>
                 <th>Выполнено</th>
                 <th >
@@ -82,19 +111,20 @@ function App() {
             </thead>
             <tbody className=''>
               {
-                todos.slice(0, 10).map((element) => {
-                  return <>
+                todos?.map((element) => {
+                  return (
                     <tr key={element.id} className='border-b-1 border-gray-200 h-[35px]'>
-                      <td className='p-0.5 border-l-1  border-gray-300 text-balance'>{element.title}</td>
+                      <td className='text-center'>{element.id}</td>
+                      <td className='p-1 border-l-1  border-gray-300 text-balance'>{element.title.slice(0, 1).toUpperCase().concat(element.title.slice(1, -1))}</td>
                       <td className=' text-center'>{element.completed ? <input type="checkbox" /> : <input type="checkbox" />}</td>
                       <td className='border-r-1 '>
-                      <button >
-                        {<Trash size={18} className='' />}
-                      </button>
+                        <button onClick={() => functionDeleteTodos(element.id)} >
+                          <Trash size={18} className='' />
+                        </button>
                       </td>
 
                     </tr>
-                  </>
+                  )
                 })
               }
             </tbody>
